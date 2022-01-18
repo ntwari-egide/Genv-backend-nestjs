84a985cc0dd58f9d685e680e25f998ec5df75645
ntwari-egide <ntwariegide2@gmail.com>
2022-01-18 19:19:51 +0200

    ft(orders): implement dtos

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,21 @@
-export class CreateOrderDto {}
+/**
+ * @author: ntwari egide
+ * @description: create order dto
+ */
+
+import { ApiProperty } from "@nestjs/swagger";
+import { CreateOrderedProductDto } from "src/ordered-products/dto/create-ordered-product.dto";
+
+export class CreateOrderDto {
+
+    @ApiProperty({
+        description: "order id"
+    })
+    orderId: String
+
+    @ApiProperty({
+        description: "all ordered products"
+    })
+    orderedProducts: [CreateOrderedProductDto]
+
+}
--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,4 +1,21 @@
-import { PartialType } from '@nestjs/swagger';
+/**
+ * @author: ntwari egide
+ * @description: update order dto
+ */
+
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { CreateOrderedProductDto } from 'src/ordered-products/dto/create-ordered-product.dto';
 import { CreateOrderDto } from './create-order.dto';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+    
+    @ApiProperty({
+        description: "order id"
+    })
+    orderId: String
+
+    @ApiProperty({
+        description: "all ordered products"
+    })
+    orderedProducts: [CreateOrderedProductDto]
+}
