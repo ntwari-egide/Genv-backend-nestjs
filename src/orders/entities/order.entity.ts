633bec46d922c5867d9ca563f051de2d3b182c9b
ntwari-egide <ntwariegide2@gmail.com>
2022-01-18 19:16:22 +0200

    ft(ordered-products): implement order schema

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,16 @@
-export class Order {}
+/**
+ * @author: ntwari egide
+ * @description: Orders Schema modal implementation
+ */
+
+import * as mongoose from 'mongoose';
+import { OrderedProductSchema } from 'src/ordered-products/entities/ordered-product.entity';
+
+
+export const OrderSchema = new mongoose.Schema ({
+
+    orderId: String,
+
+    orderedProducts : [OrderedProductSchema]
+
+})
