d49323b04818772146c96a86b53442a22102b9a7
ntwari-egide <ntwariegide2@gmail.com>
2022-01-18 19:23:31 +0200

    ft(order): update module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,31 @@
+/**
+ * @author: ntwari egide
+ * @description: orders module implementation
+ */
+
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { OrderedProductsService } from 'src/ordered-products/ordered-products.service';
+import { orderProvider } from './orders.provider';
+import { orderedProductProvider } from 'src/ordered-products/ordered-products.provider';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 300
+    })
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [
+    OrdersService,
+    OrderedProductsService,
+    ...orderProvider,
+    ...orderedProductProvider
+  ]
 })
 export class OrdersModule {}
