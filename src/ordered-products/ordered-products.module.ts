1feeda42b7ddd94dc90f26e67e48217e548ce5ad
ntwari-egide <ntwariegide2@gmail.com>
2022-01-18 14:26:17 +0200

    ft(ordered-product): configure module

--- src/ordered-products/ordered-products.module.ts
+++ src/ordered-products/ordered-products.module.ts
@@ -1,9 +1,27 @@
+/**
+ * @author: ntwari egide
+ * @description: ordered product module implementation
+ */
+
 import { Module } from '@nestjs/common';
 import { OrderedProductsService } from './ordered-products.service';
 import { OrderedProductsController } from './ordered-products.controller';
+import { orderedProductProvider } from './ordered-products.provider';
+import { DatabaseModule } from 'src/database/database.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 300
+    })
+  ],
   controllers: [OrderedProductsController],
-  providers: [OrderedProductsService]
+  providers: [
+    OrderedProductsService,
+    ...orderedProductProvider
+  ]
 })
 export class OrderedProductsModule {}
