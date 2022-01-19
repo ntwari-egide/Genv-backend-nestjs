7d72a62e846ab321aa6ed4e69361896fcaa23565
ntwari-egide <ntwariegide2@gmail.com>
2022-01-19 18:40:46 +0200

    ft(validation): implement validation

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
 
   app.use(helmet());
 
+  app.useGlobalPipes(new ValidationPipe())
 
   const config = new DocumentBuilder()
     .setTitle('GenV apis documentation')
